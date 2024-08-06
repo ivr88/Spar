402260252145380e2fe8fdaec67e0ac4e3b6a52c
ivr88 <ivr88@yandex.ru>
2024-08-06 13:39:56 +0300

    Initial Commit

---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by Вадим Исламов on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
